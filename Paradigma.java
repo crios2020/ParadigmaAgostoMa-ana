27bbff03e6902d6dadb817fc0c1c5513bc4febbd
Carlos Rios <intercrios@hotmail.com>
2021-08-24 13:02:56 -0300

    Add files via upload

---
+++ Paradigma.java
@@ -0,0 +1,74 @@
+
+//Declaración de clase
+public class Paradigma{
+
+    public static void main(String[] args){
+        //Punto de entrada
+        System.out.println("Hola Mundo!");
+
+        System.out.println("-- auto1 --");
+        //creación de un objeto
+        Auto auto1=new Auto();      // construyo un Auto
+
+        //coloco estado al objeto (valor a sus atributos)
+        auto1.marca="Ford";
+        auto1.modelo="Ka";
+        auto1.color="Negro";
+
+        //uso de métodos
+        auto1.acelerar();       // 10
+        auto1.acelerar();       // 20
+        auto1.acelerar();       // 30
+        auto1.frenar();         // 20
+
+        //imprimir estado de auto1
+        System.out.println(auto1.marca+" "+auto1.modelo+" "+auto1.color+" "+auto1.velocidad);
+
+        System.out.println("-- auto2 --");
+        Auto auto2=new Auto();
+        auto2.marca="Fiat";
+        auto2.modelo="Idea";
+        auto2.color="Gris";
+        auto2.acelerar();
+        auto2.acelerar(12);
+        System.out.println(auto2.marca+" "+auto2.modelo+" "+auto2.color+" "+auto2.velocidad);
+        
+    }
+
+} //end class Paradigma
+
+class Auto{
+
+    // Atributos
+    String marca;
+    String modelo;
+    String color;
+    int velocidad;
+
+    // Métodos
+    void acelerar(){
+        velocidad=velocidad+10;
+    }
+
+    // método con ingreso de parametro
+    void acelerar(int kilometros){
+        velocidad=velocidad+kilometros;
+    }
+
+    void frenar(){
+        velocidad=velocidad-10;
+    }
+
+} //end class Auto
+
+
+class Cuenta{
+    int nro;
+    float saldo;
+}
+
+class Cliente{
+    String nombre;
+    int edad;
+    Cuenta cuenta;
+}
